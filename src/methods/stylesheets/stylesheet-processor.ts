199fca9560de71b627f35129822012ffd130fc56
marcusds <git@marcusds.ca>
2025-09-06 22:24:25 -0700

    refactor(stylesheet): remove legacy color attributes and simplify rendering

--- src/methods/stylesheets/stylesheet-processor.ts
+++ src/methods/stylesheets/stylesheet-processor.ts
@@ -4,9 +4,6 @@ export const name = 'stylesheet-processor';
 export function render({
   default_foreground_color,
   default_background_color,
-  default_link_color,
-  default_visited_color,
-  default_active_color,
   is_toplevel,
 }: RenderOptions) {
   return `
@@ -25,25 +22,6 @@ ${'' /* #29 */}\
   color: ${default_foreground_color} !important;
 }
 
-${'' /* Legacy Attributes */}\
-[bgcolor] {
-  background-color: ${default_background_color} !important;
-}
-[text],
-[color] {
-  color: ${default_foreground_color} !important;
-}
-
-[alink]:link:active {
-  color: ${default_active_color} !important;
-}
-[vlink]:visited {
-  color: ${default_visited_color} !important;
-}
-[link]:link {
-  color: ${default_link_color} !important;
-}
-${'' /* Legacy Attributes */}\
 
 ${'' /* Bittorrent sync webui fix */}\
 .qrCode > canvas {
